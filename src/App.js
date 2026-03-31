026b30d6e00a0e602fe41e35d04d13c3114348e3
hanivepham <hanivearrozale@gmail.com>
2026-03-31 13:33:05 +0700

    test profil

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+// Ini cara manggil file profil lu yang tadi dibikin
+import HanifProfile from "./pages/hanif";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        {/* URL /hanif bakal nampilin desain dari hanif.jsx */}
+        <Route path="/hanif" element={<HanifProfile />} />
+
+        {/* Nanti kalau temen lu udah ngerjain, tambahin rutenya di bawah sini */}
+        {/* <Route path="/nama-temen" element={<ProfileTemen />} /> */}
+      </Routes>
+    </Router>
   );
 }
 
